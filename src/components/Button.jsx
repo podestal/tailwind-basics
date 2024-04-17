78b1f1534b087d24852bc57640e29355cea83d8a
podestal <luisrodriguezpodesta@gmail.com>
2024-04-17 12:14:55 -0400

    special offer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,11 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconURL, borderColor, backgroundColor, textColor }) => {
   return (
     <button 
-        className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+        className={
+            `flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
+             ${backgroundColor ? backgroundColor : 'bg-coral-red '}
+             ${textColor ? textColor : 'text-white'}
+             ${borderColor ? borderColor : 'border-coral-red'}`}>
         {label}
         {iconURL && <img className="ml-2 rounded-full w-5 h-5" src={iconURL} alt="arrow right icon" />}
     </button>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,7 +3,7 @@ import ServiceCard from "./ServiceCard"
 
 const Services = () => {
   return (
-    <section className='max-container flex justify-center flex-wrap gap-9'>
+    <section className='max-container flex flex-col justify-center flex-wrap gap-9 lg:flex-row'>
         {services.map( service => (
             <ServiceCard 
                 key={service.label}

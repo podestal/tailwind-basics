94eea5de442dff1bf1753fc531c440ebe77d8d3c
podestal <luisrodriguezpodesta@gmail.com>
2024-04-18 11:49:09 -0400

    reviews

--- src/components/CustomerReviews.jsx
+++ src/components/CustomerReviews.jsx
@@ -1,6 +1,26 @@
+import { reviews } from "../constants"
+import ReviewCard from "./ReviewCard"
+
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section className="max-container">
+        <h3 className="text-center text-4xl font-palanquin font-bold">
+            What Our
+            <span className="text-coral-red"> Customers </span>
+            Say?
+        </h3>
+        <p className="text-center mt-4 info-text m-auto max-w-lg">
+            Hear ggenuine stories from our satisfied customers about their exceptional experiences with us.
+        </p>
+        <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+            {reviews.map( review => (
+                <ReviewCard 
+                    key={review.customerName}
+                    review={review}
+                />
+            ))}
+        </div>
+    </section>
   )
 }
 
